70afa0890b259e684a502e6d73dcdfe74d05bd6c
Trend20 <enockomondi305@gmail.com>
2022-10-10 14:40:18 +0300

    adding api_key

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 
+export interface ApiResult{
+  page: number;
+  results: any[];
+  total_pages: number;
+  total_results: number;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +17,13 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
-  getTopRatedMovies(){
-    return this.http.get(`${environment.baseUrl}/movie/popular?api_key=${environment.apiKey}`);
+  // get all top rated movies
+  getTopRatedMovies(page = 1): Observable<ApiResult>{
+    return this.http.get<ApiResult>(`${environment.baseUrl}/movie/popular?api_key=${environment.apiKey}&page=${page}`);
+  }
+
+  // get movie details
+  getMovieDetails(id: string){
+    return this.http.get(`${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}`);
   }
 }
